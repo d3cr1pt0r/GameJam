f8b8dc6b186bc54547a96545df2f4e1757c45790
Klemen <klemen.brajkovic@gmail.com>
2014-12-21 21:33:42 +0100

    ...

---
+++ Assets/Resources/Scripts/BackgroundResize.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundResize : MonoBehaviour
+{
+	
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+		Vector3 leftPos = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, 5));
+		Vector3 rightPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 5));
+
+		float screenWidthWorld = rightPos.x - leftPos.x;
+		transform.localScale = new Vector3 (screenWidthWorld, screenWidthWorld, 1);
+
+		//transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, 6);
+	}
+}
