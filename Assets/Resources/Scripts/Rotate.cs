856f25cebda8a2319608e7263419518459e88227
Klemen <unknown-user@fe80::8888:a2d1:8392:8ed6%3>
2014-12-21 13:26:00 +0100

    Yeees

---
+++ Assets/Resources/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+	void Start ()
+	{
+	
+	}
+
+	void Update ()
+	{
+		transform.Rotate (new Vector3 (0, 0.1f, 0));
+	}
+}
