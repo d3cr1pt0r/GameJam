30c013c16bbbedcdbd345864f2f7a97910161761
mrpreeters <robi@pritrznik.si>
2014-12-22 02:51:23 +0100

    scene snowman fix

--- Assets/Resources/Scripts/TerrainHelper.cs
+++ Assets/Resources/Scripts/TerrainHelper.cs
@@ -5,14 +5,18 @@ using System.Collections.Generic;
 public class TerrainHelper : MonoBehaviour
 {
 	public GameObject TilePrefab;
+	public GameObject SnowmanPrefab;
 	
 	public List<Floor> floors;
+	public List<Floor> enemy;
+	
 	public Vector3 previous;
 	void Start ()
 	{
 		floors = new List<Floor> ();
 		previous = TilePrefab.transform.position;
-		generateTiles (30);
+		generateTiles (10);
+		spawnSnowman ();
 		//generateTile ();
 		//generateTile ();
 		
@@ -23,35 +27,52 @@ public class TerrainHelper : MonoBehaviour
 		if (Vector3.Distance (new_pos, prev_pos) < 5f)
 		{
 			return true;
-
+			
 		}
 		return false;
 	}
-
-
+	
+	void spawnSnowman()
+	{
+		Random.seed = (int)System.DateTime.Now.Ticks;
+		
+		for (int i = 0; i< floors.Count; i++) {
+			float propability = Random.Range (0,floors.Count);
+			
+			if(propability%3 == 0)
+			{
+				Vector3[] pos = floors[i].GetPoints();
+				
+				pos[1].y +=0.75f;
+				
+				floors.Add (CreateEnemy(new Vector3(pos[1].x,pos[1].y, 5)));
+			}
+		}
+	}
+	
 	void generateTiles(int num)
 	{
 		previous = TilePrefab.transform.position;
 		Vector3 new_pos;
-
+		
 		Random.seed = (int)System.DateTime.Now.Ticks;
 		float max_up = 2.5f;
 		float max_side = 1.5f;
-
+		
 		for (int i =0; i<num; i++)
 		{
 			Vector3 borderPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height/2.0f,5));
 			
 			float max_x= Random.Range (0-borderPos.x-max_side,borderPos.x+max_side);
 			float max_y= Random.Range (max_x, previous.y+max_side);
-
+			
 			int length = (int)Random.Range (9f, 17f);
-
+			
 			new_pos = new Vector3(max_x, previous.y+max_up, 5);
-
+			
 			float dist = Vector3.Distance(previous, new_pos);
-			//Debug.Log (dist);
-
+			Debug.Log (dist);
+			
 			while(!inRange(new_pos,previous))
 			{
 				if(new_pos.x < previous.x)
@@ -61,14 +82,14 @@ public class TerrainHelper : MonoBehaviour
 				else
 				{
 					new_pos.x-= 2.5f;
-
+					
 				}
 			}
 			floors.Add (CreateFloor(new_pos, length));
-
+			
 			previous.y += max_up;
-
-
+			
+			
 		}
 	}
 	
@@ -87,4 +108,8 @@ public class TerrainHelper : MonoBehaviour
 		return new Floor (TilePrefab, pos, length);
 	}
 	
+	public Floor CreateEnemy(Vector3 pos)
+	{
+		return new Floor (SnowmanPrefab, pos);
+	}
 }
